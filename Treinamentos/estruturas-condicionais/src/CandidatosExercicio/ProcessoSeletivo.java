1107cbfb55f9fe5cf110e83566c764b916a83d75
Dijael Ramos <dijaelr@gmail.com>
2023-12-27 16:55:46 -0300

    add

---
+++ Treinamentos/estruturas-condicionais/src/CandidatosExercicio/ProcessoSeletivo.java
@@ -0,0 +1,43 @@
+package CandidatosExercicio;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class ProcessoSeletivo {
+    public static void main(String[] args) {
+            selecaoCanditatos();
+
+    }  
+    static void selecaoCanditatos() {
+        String [] candidatos = {"FELIPE", "MARCIA", "JONAS", "JULIA", "MARCOS", "AUGUSTO", "MONICA", "FABRICIO", "MIRELA", "DANIELA"};
+        
+        int candidatosSelecionados = 0;
+        int candidatoAtual = 0;
+        double salarioBase = 2000.0;
+
+        while (candidatosSelecionados < 5 && candidatoAtual < candidatos.length) {
+            String candidato = candidatos [candidatoAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("O candidato " + candidato + " solicitou este valor de salário " + salarioBase);
+            if (salarioBase >= salarioPretendido) {
+                System.out.println("O candidato " + candidato + " foi selecionado para a vaga.");
+                candidatosSelecionados++;
+            } 
+            candidatoAtual++;
+        }
+    } 
+    static double valorPretendido() {
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
+    }
+
+    static void analisarCandidato(double salarioPretendido) {
+        double salarioBase = 2000.0;
+        if (salarioBase > salarioPretendido) {
+            System.out.println("LIGAR PARA O CANDIDATO!");
+        } else if (salarioBase==salarioPretendido) {
+            System.out.println("LIGAR PARA O CANDIDATO COM CONTRA PROPOSTA");
+        } else {
+            System.out.println("AGUARDANDO O RESULTADO DOS DEMAIS CANDIDATOS");
+        }
+    }
+}
