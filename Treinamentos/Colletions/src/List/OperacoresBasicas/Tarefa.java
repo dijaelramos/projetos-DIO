073e150f2620238abf430209977cb2efb08e3ee1
Dijael Ramos <dijaelr@gmail.com>
2024-01-09 14:32:45 -0300

    Primeiros comandos
    Signed-off-by: Dijael Ramos <dijaelr@gmail.com>

---
+++ Treinamentos/Colletions/src/List/OperacoresBasicas/Tarefa.java
@@ -0,0 +1,19 @@
+package List.OperacoresBasicas;
+
+public class Tarefa {
+    //Atributos
+    private String descricao;
+
+    
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
+
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    
+
+}
