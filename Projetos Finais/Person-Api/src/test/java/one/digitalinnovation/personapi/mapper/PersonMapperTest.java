129a6d0912ecfe228c87633ab171c8ff6e84f3aa
Dijael Ramos <dijaelr@gmail.com>
2024-03-14 11:56:31 -0300

    Gerenciamento de pessoas de uma empresa através de uma API REST

---
+++ Projetos Finais/Person-Api/src/test/java/one/digitalinnovation/personapi/mapper/PersonMapperTest.java
@@ -0,0 +1,60 @@
+package one.digitalinnovation.personapi.mapper;
+
+import one.digitalinnovation.personapi.dto.mapper.PersonMapper;
+import one.digitalinnovation.personapi.dto.request.PersonDTO;
+import one.digitalinnovation.personapi.dto.request.PhoneDTO;
+import one.digitalinnovation.personapi.entities.Person;
+import one.digitalinnovation.personapi.entities.Phone;
+import one.digitalinnovation.personapi.utils.PersonUtils;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class PersonMapperTest {
+
+    @Autowired
+    private PersonMapper personMapper;
+
+    @Test
+    //testar DTO da pessoa dada e, em seguida, retornar entidade de pessoa
+    void testGivenPersonDTOThenReturnPersonEntity() {
+        PersonDTO personDTO = PersonUtils.createFakeDTO();
+        Person person = personMapper.toModel(personDTO);
+
+        assertEquals(personDTO.getFirstName(), person.getFirstName());
+        assertEquals(personDTO.getLastName(), person.getLastName());
+        assertEquals(personDTO.getCpf(), person.getCpf());
+
+        assertEquals(personDTO.getStreet(), person.getStreet());
+        assertEquals(personDTO.getZipCode(), person.getZipCode());
+
+        Phone phone = person.getPhones().get(0);
+        PhoneDTO phoneDTO = personDTO.getPhones().get(0);
+
+        assertEquals(phoneDTO.getType(), phone.getType());
+        assertEquals(phoneDTO.getNumber(), phone.getNumber());
+    }
+
+    @Test
+    //Testar a Entidade da Pessoa Dada e, em seguida, Retorne a DTO da Pessoa
+    void testGivenPersonEntityThenReturnPersonDTO() {
+        Person person = PersonUtils.createFakeEntity();
+        PersonDTO personDTO = personMapper.toDTO(person);
+
+        assertEquals(person.getFirstName(), personDTO.getFirstName());
+        assertEquals(person.getLastName(), personDTO.getLastName());
+        assertEquals(person.getCpf(), personDTO.getCpf());
+
+        assertEquals(person.getStreet(), personDTO.getStreet());
+        assertEquals(person.getZipCode(), personDTO.getZipCode());
+
+        Phone phone = person.getPhones().get(0);
+        PhoneDTO phoneDTO = personDTO.getPhones().get(0);
+
+        assertEquals(phone.getType(), phoneDTO.getType());
+        assertEquals(phone.getNumber(), phoneDTO.getNumber());
+    }
+}
